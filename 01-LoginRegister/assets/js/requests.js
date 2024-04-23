e97a8c978a5b54a1dff535428c02f3d2b70ba4ed
gurbangurbanzade <gurban.gurbanzadeh@gmail.com>
2024-04-23 14:48:33 +0400

    login register

---
+++ 01-LoginRegister/assets/js/requests.js
@@ -0,0 +1,9 @@
+export const getAllData = async (url) => {
+  const res = await axios(url);
+  const data = res.data;
+  return data;
+};
+
+export const postData = async (url, obj) => {
+  const res = await axios.post(url, obj);
+};
